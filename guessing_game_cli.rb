20fb859a4bec3b284fa1e695d57c74ad501a4aad
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-11-15 20:32:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   
   #puts "Guess a number between 1 and 6!"
-  random_num = rand(1..6).to_s
+  random_num = (rand(6) + 1).to_s
   user_input = gets.chomp
   
   if user_input == "exit"
